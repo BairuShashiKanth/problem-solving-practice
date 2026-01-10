5ae73975abe390e5f97481313874074fe79bd78f
BairuShashiKanth <69531820+BairuShashiKanth@users.noreply.github.com>
2026-01-10 23:18:27 +0530

    Time: 29 ms (2.96%), Space: 47 MB (9.61%) - LeetHub

---
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -0,0 +1,46 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        ListNode resultList,temp;
+        int sum,l1Value,l2Value,result,prevCarryDigit = 0;
+        
+        l1Value = (l1 != null) ? l1.val : 0;
+        l2Value = (l2 != null) ? l2.val : 0;
+        sum = l1Value + l2Value + prevCarryDigit;
+        result = (sum) % 10 ;
+        resultList = new ListNode(result);
+        prevCarryDigit=sum/10;
+        if (l1 != null ) l1 = l1.next;
+        if (l2 != null ) l2 = l2.next;
+        
+        temp = resultList;
+        
+        while (l1 != null || l2 != null || prevCarryDigit>0)
+        {
+            l1Value = (l1 != null) ? l1.val : 0;
+            l2Value = (l2 != null) ? l2.val : 0;
+            sum = l1Value + l2Value + prevCarryDigit;
+            result = (sum) % 10 ;
+            System.out.println("result:"+result);
+ 
+            temp.next = new ListNode(result);
+            temp = temp.next;
+            
+            prevCarryDigit=sum/10;
+
+            if (l1 != null ) l1 = l1.next;
+            if (l2 != null ) l2 = l2.next;
+        }
+
+        return resultList;
+    }
+}
