92d36eff5f5bacf7e59ab58f54a53e385b0b24f8
BairuShashiKanth <69531820+BairuShashiKanth@users.noreply.github.com>
2026-01-16 00:08:50 +0530

    Time: 0 ms (100%), Space: 44.4 MB (29.56%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -11,32 +11,29 @@
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
         
-        if(head == null || head.next == null) return head;
-        ListNode current = head, lastNodeTemp, newHead;
+        if(head == null || head.next == null || k == 0) return head;
+        ListNode tail = head, newTail, newHead;
 
-        int listNodeLength = 0;
-        while(current != null){
-            current = current.next;
-            listNodeLength++;
+        int length = 1;
+        while(tail.next != null){
+            tail = tail.next;
+            length++;
         }
 
-        k%= listNodeLength;
+        k%=length;
+
         if ( k == 0 ) return head;
-        current = head;
+        
+        tail.next = head; //making it a circular linkedList.
 
-        for(int i=0;i<listNodeLength-k-1;i++){
-            current = current.next;
-        }
-        lastNodeTemp = current;
-        newHead = current.next;
-        while(current.next != null){
-            current = current.next;
-        }
+        int stepsToNewHead = length - k - 1;
+        newTail = head;
+        while(stepsToNewHead-- > 0)
+            newTail = newTail.next;
+        
+        newHead = newTail.next;
+        newTail.next = null;
 
-        current.next = head;
-        head = newHead;
-        lastNodeTemp.next = null;
-
-        return head;
+        return newHead;
     }
 }
